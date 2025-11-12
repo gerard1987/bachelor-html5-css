3d35f60ae537a5fc54519fab3130f82088cd53b0
Gerard <gerard.deway@vwe.nl>
2025-11-12 10:43:50 +0100

    Header components, active link, pages, styling

--- js/main.js
+++ js/main.js
@@ -1,45 +1,65 @@
-const componentLocation = "./components/";
+const componentLocation = "../components/";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadComponents();
-  toggleNav();
+  loadComponents().then(() => {
+    toggleResponsiveNav();
+    setHeaderActiveLink();
+  });
 });
 
-function loadComponents(){
-    document.querySelectorAll('.component').forEach(element => {
-      const component = {
-        element: element,
-        url: componentLocation + element.id + ".html"
-      }
+function loadComponents() {
+  const promises = [];
 
-      renderComponent(component);
+  document.querySelectorAll(".component").forEach((element) => {
+    const component = {
+      element: element,
+      tagName: element.tagName.toLowerCase(),
+      url: componentLocation + element.dataset.component + ".html",
+    };
+
+    promises.push(renderComponent(component));
   });
+
+  return Promise.all(promises);
 }
 
 function renderComponent(component) {
-  try {
-    fetch(component.url).then(function (response) {
+  return fetch(component.url)
+    .then(function (response) {
       if (response.ok) {
         return response.text();
       }
 
       throw response;
-    }).then(function (text) {
-      component.element.innerHTML = text
+    })
+    .then(function (text) {
+      const template = document.createElement("template");
+      template.innerHTML = text.trim();
+      component.element.replaceWith(template.content.firstChild);
+    })
+    .catch(function (error) {
+      console.error(error.message);
     });
-  }
-  catch (error) {
-    console.error(error.message);
-  }
 }
 
-function toggleNav(){
-    const toggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('.main-nav');
+function setHeaderActiveLink() {
+  document.querySelectorAll(".main-nav > a").forEach((element) => {
+    if (element.pathname == window.location.pathname) {
+      element.classList.add("active");
+    }
+  });
+}
 
-    toggle.addEventListener('click', () => {
-      const isOpen = nav.classList.toggle('open');
-      toggle.setAttribute('aria-expanded', isOpen);
-      toggle.setAttribute('aria-label', isOpen ? 'Close main menu' : 'Open main menu');
-    });
+function toggleResponsiveNav() {
+  const toggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".main-nav");
+
+  toggle.addEventListener("click", () => {
+    const isOpen = nav.classList.toggle("open");
+    toggle.setAttribute("aria-expanded", isOpen);
+    toggle.setAttribute(
+      "aria-label",
+      isOpen ? "Close main menu" : "Open main menu"
+    );
+  });
 }
